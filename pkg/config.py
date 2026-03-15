8ed9dec1823c6e246614a10de62ead737f1c02ae
Stanley <stanley@moatcohorts.com.ng>
2026-03-15 04:17:25 +0100

    Updated requirements and config.py file again

--- pkg/config.py
+++ pkg/config.py
@@ -1,6 +1,6 @@
 class General(object):
     APP_NAME="DevFest App"
-    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://freedb_devfest_user:hFyqa3G&XFE5Mf3@sql.freedb.tech/freedb_devfest_db'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://freedb_devfest_user:hFyqa3G&XFE5Mf3@sql.freedb.tech/freedb_devfest_db'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class TestConfig(General):
