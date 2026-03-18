ed52dfb9cbb780f1f2c2220c09602423710b4dc3
Stanley <stanley@moatcohorts.com.ng>
2026-03-18 11:55:09 +0100

    Updated config.py by removing pymysql

--- pkg/config.py
+++ pkg/config.py
@@ -1,6 +1,6 @@
 class General(object):
     APP_NAME="DevFest App"
-    SQLALCHEMY_DATABASE_URI='postgresql+pymysql://devfest_db_x2af_user:y4sDsBVjrjKL1KBpkB04AURUdJBvHNL8@dpg-d6t652vkijhs73etmspg-a/devfest_db_x2af'
+    SQLALCHEMY_DATABASE_URI='postgresql://devfest_db_x2af_user:y4sDsBVjrjKL1KBpkB04AURUdJBvHNL8@dpg-d6t652vkijhs73etmspg-a/devfest_db_x2af'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class TestConfig(General):
